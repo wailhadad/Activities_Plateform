a3843486d0097da7f80c6a8391631188a22711c9
WAIL1221 <wailw2445@gmail.com>
2024-06-10 15:00:20 +0200

    97th Commit From Wail

--- Frontend/src/components/OffersList.vue
+++ Frontend/src/components/OffersList.vue
@@ -71,7 +71,7 @@ export default {
     fetchOffers() {
       this.loading = true;
       this.error = null;
-      axios.get(`${process.env.VUE_APP_API_URL}/api/show/offers`)
+      axios.get(`http://16.171.10.244:8000/api/show/offers`)
         .then(response => {
           this.offers = response.data;
           this.loading = false;
